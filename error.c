044fa99d611aaa4dc3cc807086bfcb96c808aeb7
gui-co <guillaume.communie@gmail.com>
2018-02-05 18:06:18 +0100

    correction switch

--- error.c
+++ error.c
@@ -22,12 +22,17 @@ void end(int err)
   switch (err)
   {
     case E_FILE_NOT_FOUND : fprintf(stderr,"Unable to open %s. File not found!\n",errorData);
+                            exit(1);
     
     case E_FILE_NOT_CORRECT : fprintf(stderr,"File %s not correct!\n",errorData);
+                              exit(1);
     
     case E_MALLOC : fprintf(stderr,"Memory allocation error!\n");
+                    exit(1);
     
     case E_FREAD : fprintf(stderr,"Error while reading %s file!\n",errorData);
+                   exit(1);
+
+    default : exit(1);
   }
-    exit(1);
 }
